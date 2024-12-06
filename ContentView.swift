69a25f9895173c032ae0e686a3f1226462a796b2
Sudharaka Ashen <sudharakaashenaudi@icloud.com>
2024-12-06 20:36:58 +0530

    Initial commit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Restaurent Menu App
+//
+//  Created by Sudharaka Ashen Edussuriya on 2024-12-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Restaurent_Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurent_Menu_AppApp.swift
+//  Restaurent Menu App
+//
+//  Created by Sudharaka Ashen Edussuriya on 2024-12-06.
+//
+
+import SwiftUI
+
+@main
+struct Restaurent_Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
